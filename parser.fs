4c46155dad908d4de4cf5b6676f2b7b5a9390d9b
darkf <lw9k123@gmail.com>
2011-11-30 13:35:41 -0600

    add escape codes to string parsing

--- parser.fs
+++ parser.fs
@@ -24,16 +24,30 @@ let parseString (str : string) =
   let mutable acc = ""
   let mutable inString = false
   let mutable inAtom = false
+  let mutable inEscape = false
   let mutable xs = []
 
   for i in 0..str.Length-1 do
     match str.[i] with
+      // escape codes
+      | '\\' when inString = true -> inEscape <- true
+      | 'n' | 'r' | 't' | '"' | '\\' when inEscape = true ->
+        acc <- acc + (match str.[i] with
+                        | 'n' -> "\n"
+                        | 'r' -> "\r"
+                        | 't' -> "\t"
+                        | '"' -> "\""
+                        | '\\' -> "\\" | _ -> "")
+        inEscape <- false
+      // end string
       | '"' when inString = true ->
         xs <- xs @ [StringNode acc]
         inString <- false
         acc <- ""
+      // begin string
       | '"' when inString = false && inAtom = false ->
         inString <- true
+      // whitespace
       | ' ' | '\t' | '\r' | '\n' when inString = false ->
         if acc.Length > 0 then
           xs <- xs @ [getNode acc]
