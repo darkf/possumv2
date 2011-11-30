f33e569a597b300e262733d6deb0b9778815d0da
darkf <lw9k123@gmail.com>
2011-11-29 23:12:55 -0600

    implement stream-read

--- stream.fs
+++ stream.fs
@@ -7,6 +7,16 @@ let fileOpen (args : expr list) : expr =
       StreamNode (new System.IO.FileStream(path, System.IO.FileMode.OpenOrCreate))
     | _ -> raise (SemanticError "non-path passed to file-open")
 
+let streamRead (args : expr list) : expr =
+  match args.[0] with
+    | StreamNode s ->
+      let n = exprToInt args.[1]
+      let buf : byte[] = Array.zeroCreate n
+      match s.Read(buf, 0, n) with
+        | 0 -> NilNode // EOF
+        | c -> StringNode (System.Text.Encoding.UTF8.GetString(buf, 0, n))
+    | _ -> raise (SemanticError "non-stream passed to stream-read")
+
 let streamReadAll (args : expr list) : expr =
   match args.[0] with
     | StreamNode s ->
@@ -23,5 +33,6 @@ let streamClose (args : expr list) : expr =
 let initModule (sym : ExprDict) =
   sym.["file-open"] <- FunctionNode ("file-open", 1, fileOpen)
 
+  sym.["stream-read"] <- FunctionNode ("stream-read", 2, streamRead)
   sym.["stream-read-all"] <- FunctionNode ("stream-read-all", 1, streamReadAll)
   sym.["stream-close"] <- FunctionNode ("stream-close", 1, streamClose)
