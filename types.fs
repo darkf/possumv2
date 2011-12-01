416e2342b0bc149291d6ec7cef3375b4b406d9d3
darkf <lw9k123@gmail.com>
2011-11-30 22:02:39 -0600

    clean up types and fix (I mean hack) defun not working

--- types.fs
+++ types.fs
@@ -10,39 +10,52 @@ type expr = AtomNode of string
           | BoolNode of bool
           | NilNode
 
+          with
+          override x.ToString() =
+            match x with
+              | AtomNode s -> s
+              | _ -> "fixme"
+
 exception ParseError of string
 exception BindingError of string * string
 exception SemanticError of string
 
 type ExprDict = System.Collections.Generic.Dictionary<string, expr>
 
-let exprToString e =
+let rec exprToString e =
   match e with
-    (AtomNode s) -> sprintf "<atom '%s'>" s
-  | (StringNode s) -> sprintf "<str '%s'>" s
-  | (IntegerNode i) -> sprintf "<int %d>" i
-  | (FunctionNode (name, _, _)) -> sprintf "<fn '%s'>" name
-  | BoolNode b -> sprintf "<bool %s>" (if b = true then "true" else "false")
-  | NilNode -> sprintf "<nil>"
-  //| _ -> sprintf "<<<error>>> -> %s" (string e)
+    | AtomNode s -> s
+    | StringNode s -> sprintf "<str '%s'>" s
+    | IntegerNode i -> sprintf "<int %d>" i
+    | FunctionNode (name, _, _) -> sprintf "<fn '%s'>" name
+    | BoolNode b -> sprintf "<bool %s>" (if b then "true" else "false")
+    | PairNode (a, b) -> sprintf "<pair %s -> %s>" (exprToString a) (exprToString b)
+    | StreamNode s -> "<stream>"
+    | NilNode -> sprintf "<nil>"
+    //| _ -> sprintf "<<<error>>> -> %s" (string e)
+
+let exprRepr e =
+  match e with
+    | AtomNode s -> sprintf "<atom '%s'>" s
+    | _ -> exprToString e
 
 let exprToInt (e : expr) : int =
   match e with
-    (IntegerNode i) -> i
-  | _ -> -1
+    |  IntegerNode i -> i
+    | _ -> -1
 
 let exprToBool (e : expr) : bool =
   match e with
-    (BoolNode b) -> b
-  | _ -> false
+    | BoolNode b -> b
+    | _ -> false
 
 let exprEquals (x : expr) (y : expr) : bool =
-  match (x, y) with
-    | (IntegerNode a, IntegerNode b) -> a = b
-    | (StringNode a, StringNode b) -> a = b
-    | (BoolNode a, BoolNode b) -> a = b
-    | (AtomNode a, AtomNode b) -> a = b
-    | (NilNode, NilNode) -> true
-    | (a, b) ->
+  match x, y with
+    | IntegerNode a, IntegerNode b -> a = b
+    | StringNode a, StringNode b -> a = b
+    | BoolNode a, BoolNode b -> a = b
+    | AtomNode a, AtomNode b -> a = b
+    | NilNode, NilNode -> true
+    | a, b ->
       printfn "!!! warning: equals called on (%A,%A) - returning false" a b
       false
