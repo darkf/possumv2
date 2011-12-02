d4a3d05492460be9a3d30a40745ba7fff6c2fdfd
darkf <lw9k123@gmail.com>
2011-12-02 12:14:48 -0600

    move stream.fs and text.fs to lib/

---
+++ lib/text.fs
@@ -0,0 +1,24 @@
+﻿module PossumText
+
+open System.Text.RegularExpressions
+open Types
+
+let regexMatch (args : expr list) : expr =
+  // regex-match "d.*" "ducks"
+  // returns a list of match groups
+  match args with
+    StringNode regex :: StringNode input :: [] ->
+      let m = Regex.Match(input, regex)
+      if not m.Success then
+        NilNode
+      else
+        // ugly hack to create a possum list (cons in reverse order)
+        let mutable x = NilNode
+        for i = m.Groups.Count-1 downto 0 do
+          x <- PairNode (StringNode m.Groups.[i].Value, x)
+        x
+    | _ -> raise (SemanticError "regex-match is string -> string")
+
+
+let initModule (sym : ExprDict) =
+  sym.["regex-match"] <- FunctionNode ("regex-match", 2, regexMatch)
